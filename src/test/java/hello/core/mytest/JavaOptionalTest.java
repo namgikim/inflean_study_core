d18cbe9635030bea67148de0ec38e90d3f6efa92
namgi <namgiu9@gmail.com>
2022-04-03 21:49:13 +0900

    좋아요_기능_개편 2차_1

--- src/test/java/hello/core/mytest/JavaOptionalTest.java
+++ src/test/java/hello/core/mytest/JavaOptionalTest.java
@@ -42,6 +42,6 @@ public class JavaOptionalTest {
         System.out.println("좋아요_기능_개편 1차_4");
         System.out.println("좋아요_기능_개편 1차_7");
         System.out.println("좋아요_기능_개편 1차_9");
-        System.out.println("좋아요_기능_개편 2차");
+        System.out.println("좋아요_기능_개편 2차_1");
     }
 }
